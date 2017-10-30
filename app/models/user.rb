f1942e8c7cd38d502c36ec008722632b618634e7
nkiru onwuneme <generalhrh@gmail.com>
2017-10-30 01:48:43 -0700

    Added login methd

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
 
   def self.from_auth_hash(provider, auth_hash)
-    user = new
+    user = new #can say new, because currently inside of Capital U user, find it on the current class
     user.provider = provider
     user.uid = auth_hash['uid']
     user.name = auth_hash['info']['name']
     user.email = auth_hash['info']['email']
     user.username = auth_hash['info']['nickname']
 
+    # user.save ##controller
     return user
   end
 end

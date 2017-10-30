bd15e1d7d79dffd51c15b617d8683fa09ae22aaf
nkiru onwuneme <generalhrh@gmail.com>
2017-10-29 23:21:20 -0700

    completed wave1

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,7 +2,8 @@ class WorksController < ApplicationController
   # We should always be able to tell what category
   # of work we're dealing with
   before_action :category_from_work, except: [:root, :index, :new, :create]
-
+  before_action :require_login, except: [:root, :index, :show]
+  
   def root
     @albums = Work.best_albums
     @books = Work.best_books
